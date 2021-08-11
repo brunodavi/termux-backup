edc8df1f73018cf8e439c95f0dc5efa5b30a0e58
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2021-08-11 16:56:53 -0300

    Add files via upload

---
+++ termux-backup.sh
@@ -0,0 +1,36 @@
+if [ "$#" -eq 0 ]
+then
+	echo need a backup file or folder
+	exit 1
+fi
+
+times=$(date | grep -Po '\d+:\d+:\d+'| sed s/:/-/g)
+dir=$(grep -Po '^/.+/[^/]+' <<< "$(realpath $1)")
+
+cd /data/data/com.termux/files/
+
+backup() {
+	tar_file="$dir/termux_backup_$times.tar.gz"
+	tar -zcf "$tar_file" home usr
+}
+
+restore() {
+	tar_file="$dir"
+	tar -zxf "$tar_file" \
+		--recursive-unlink \
+		--preserve-permissions
+}
+
+if [ -d "$dir" ]
+then
+	backup
+	echo backup created
+elif [ -e "$dir" ]
+then
+	restore
+	echo backup recovered
+else
+	echo invalid file or directory
+fi
+
+cd - &> /dev/null
